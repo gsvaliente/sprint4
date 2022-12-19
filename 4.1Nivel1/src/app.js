ad1a5ec9938f2a0f63c4066aa96cea2260be0129
Gabriel Valiente <gsvalient@gmail.com>
2022-12-19 09:24:27 +0100

    minor changes

--- 4.1Nivel1/src/app.js
+++ 4.1Nivel1/src/app.js
@@ -4,10 +4,13 @@ const database = require('./db/users.json');
 const app = express();
 const port = 3000;
 
+//endpoint for the user page
 app.get('/user', (req, res) => {
   res.status(200).json(database);
+  res.end();
 });
 
+//endpoint for the upload
 app.all('*', (req, res) => {
   res.status(404).send('PAGE DOES NOT EXIST');
 });
